5eca15b7c6c91ef8a91a0e930977ed70220e168b
Shadman Ahmed <shadman.ahmed098@gmail.com>
2022-01-25 10:30:22 +0000

    Added while loop on Fizzbuzz and fixed an input check bug

--- 03_Control_Flow/03_FizzBuzz.py
+++ 03_Control_Flow/03_FizzBuzz.py
@@ -12,38 +12,40 @@
 # Or the start number?
 # Can we get those from player input?
 # Can we input alternate words for fizz and buzz?
+prompt_start_end = True
+while prompt_start_end:
+    # Asks user for start and end point
+    start = input('Please input a start number: ')
+    end = input('Please input an end number: ')
 
-# Asks user for start and end point
-start = input('Please input a start number: ')
-end = input('Please input an end number: ')
+    # checks if user given values are digits
+    if start.isdigit() and end.isdigit():
+        prompt_start_end = True
+        fizz = 'fizz'
+        buzz = 'buzz'
+        # asks user if he wants to change fizzbuzz
+        response = input('do you want to customize the words fizzbuzz? (y/N): ').lower()
+        # checks user's decision, and gives the option if he wants to mod the game, if the user is trying to break the code, it doesn't let him play the game
+        if response not in ('y', 'n'):
+            print('I assume that means you don\'t actually want to play')
+        else:
+            if response == 'y':
+                fizz = input('Please type what you want fizz to be: ')
+                buzz = input('Please type what you want buzz to be: ')
 
-# checks if user given values are digits
-if start.isdigit() or end.isdigit():
-    fizz = 'fizz'
-    buzz = 'buzz'
-    # asks user if he wants to change fizzbuzz
-    response = input('do you want to customize the words fizzbuzz? (y/N): ').lower()
-    # checks user's decision, and gives the option if he wants to mod the game, if the user is trying to break the code, it doesn't let him play the game
-    if response not in ('y', 'n'):
-        print('I assume that means you don\'t actually want to play')
+            # simple fizzbuzz concatenation just in case user modded the gamedsf
+            fizzbuzz = fizz + buzz
+
+            # fizzbuzz game
+            for number in range(int(start), int(end) + 1):
+                if number % 15 == 0:
+                    print(fizzbuzz)
+                elif number % 3 == 0:
+                    print(fizz)
+                elif number % 5 == 0:
+                    print(buzz)
+                else:
+                    print(number)
     else:
-        if response == 'y':
-            fizz = input('Please type what you want fizz to be: ')
-            buzz = input('Please type what you want buzz to be: ')
-
-        # simple fizzbuzz concatenation just in case user modded the game
-        fizzbuzz = fizz + buzz
-
-        # fizzbuzz game
-        for number in range(int(start), int(end) + 1):
-            if number % 15 == 0:
-                print(fizzbuzz)
-            elif number % 3 == 0:
-                print(fizz)
-            elif number % 5 == 0:
-                print(buzz)
-            else:
-                print(number)
-else:
-    # message to user if user inputted inappropriate start and end point
-    print('\nPlease use sensible start and end point \nNo FizzBuzz for you')
+        # message to user if user inputted inappropriate start and end point
+        print('\nPlease use sensible start and end point')
